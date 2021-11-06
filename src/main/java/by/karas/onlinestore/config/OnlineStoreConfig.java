672df28ccab989c9c32e4d299aef1c84b39ba154
tikhtmp <57909756+tikhtmp@users.noreply.github.com>
2021-11-06 09:56:38 +0300

    JDBC template using

--- src/main/java/by/karas/onlinestore/config/OnlineStoreConfig.java
+++ src/main/java/by/karas/onlinestore/config/OnlineStoreConfig.java
@@ -6,12 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("by.karas.onlinestore")
@@ -46,6 +50,21 @@ public class OnlineStoreConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource =new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/onlinestore_db");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Rybanka@08");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(dataSource());
     }
 }
 
--- src/main/java/by/karas/onlinestore/dao/ProductDAO.java
+++ src/main/java/by/karas/onlinestore/dao/ProductDAO.java
@@ -1,134 +1,76 @@
 package by.karas.onlinestore.dao;
 
 import by.karas.onlinestore.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class ProductDAO {
-    private List<Product> products;
-    private static long PEOPLE_COUNT = 3;
-
-    private static final String URL = "jdbc:mysql://localhost:3306/onlinestore_db";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "Rybanka@08";
-    private static Connection connection;
-
-    static {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public ProductDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-
     public List<Product> index() {
-        List<Product> products = new ArrayList<>();
-
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "select * from products";
-            ResultSet resultSet = statement.executeQuery(SQL);
-
-            while (resultSet.next()) {
-                Product product = new Product();
-                product.setId(resultSet.getLong("id"));
-                product.setName(resultSet.getString("name"));
-                product.setShortDescription(resultSet.getString("short_description"));
-                product.setDetailDescription(resultSet.getString("detail_description"));
-                product.setPrice(resultSet.getBigDecimal("price"));
-
-                products.add(product);
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return products;
+        return jdbcTemplate.query("select * from products", new ProductMapper());
     }
 
     public Product show(Long id) {
-        Product product = null;
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "select * from products where id=?");
-
-            preparedStatement.setLong(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-
-            product = new Product();
-
-            product.setId(resultSet.getLong("id"));
-            product.setName(resultSet.getString("name"));
-            product.setShortDescription(resultSet.getString("short_description"));
-            product.setDetailDescription(resultSet.getString("detail_description"));
-            product.setPrice(resultSet.getBigDecimal("price"));
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return product;
+        return jdbcTemplate.query("select * from products where id=?", new Object[]{id}, new ProductMapper())
+                .stream().findAny().orElse(null);
     }
 
     public void save(Product product) {
 
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "insert into products values(?, ?, ?, ?, ?)");
+//        try {
+//            PreparedStatement preparedStatement = connection.prepareStatement(
+//                    "insert into products values(?, ?, ?, ?, ?)");
+//
+//            preparedStatement.setLong(1, product.getId());
+//            preparedStatement.setString(2, product.getName());
+//            preparedStatement.setString(3, product.getShortDescription());
+//            preparedStatement.setString(4, product.getDetailDescription());
+//            preparedStatement.setBigDecimal(5, product.getPrice());
+//
+//            preparedStatement.executeUpdate();
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-            preparedStatement.setLong(1, product.getId());
-            preparedStatement.setString(2, product.getName());
-            preparedStatement.setString(3, product.getShortDescription());
-            preparedStatement.setString(4, product.getDetailDescription());
-            preparedStatement.setBigDecimal(5, product.getPrice());
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void update(Long id, Product updatedProduct) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("update products set name=?, short_description=?, detail_description=?, price=? where id=?");
-
-            preparedStatement.setString(1, updatedProduct.getName());
-            preparedStatement.setString(2, updatedProduct.getShortDescription());
-            preparedStatement.setString(3, updatedProduct.getDetailDescription());
-            preparedStatement.setBigDecimal(4, updatedProduct.getPrice());
-            preparedStatement.setLong(5, id);
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void delete(Long id) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("delete from products where id=?");
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
+//    public void update(Long id, Product updatedProduct) {
+//        try {
+//            PreparedStatement preparedStatement =
+//                    connection.prepareStatement("update products set name=?, short_description=?, detail_description=?, price=? where id=?");
+//
+//            preparedStatement.setString(1, updatedProduct.getName());
+//            preparedStatement.setString(2, updatedProduct.getShortDescription());
+//            preparedStatement.setString(3, updatedProduct.getDetailDescription());
+//            preparedStatement.setBigDecimal(4, updatedProduct.getPrice());
+//            preparedStatement.setLong(5, id);
+//            preparedStatement.executeUpdate();
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public void delete(Long id) {
+//        try {
+//            PreparedStatement preparedStatement =
+//                    connection.prepareStatement("delete from products where id=?");
+//            preparedStatement.setLong(1, id);
+//            preparedStatement.executeUpdate();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
     }
 }
